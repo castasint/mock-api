16031d4c9201be61e95d8499c36182eb7f3a73b2
Hanuma <castasstring@gmail.com>
2022-09-27 22:31:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import { userPreference, referenceData } from "./data.js";
+
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/reference-data", async (request, reply) => {
+  return { data: referenceData };
+});
+
+// Declare a route
+fastify.get("/preferences", async (request, reply) => {
+  return { data: userPreference };
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
