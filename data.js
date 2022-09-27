30fbdbeb887260a1e7b0207e1949b217ceeb845d
Hanuma <castasstring@gmail.com>
2022-09-27 22:30:36 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,48 @@
+export const referenceData = {
+  countries: [
+    { code: "SG", value: "Singapore" },
+    { code: "IN", value: "India" },
+    { code: "US", value: "United States of America" },
+  ],
+  currencies: [
+    { code: "USD", value: "US Dollar" },
+    { code: "SGD", value: "Singapore Dollar" },
+    { code: "INR", value: "Indian Rupee" },
+  ],
+  languages: [
+    { code: "EN", value: "English" },
+    { ode: "FR", value: "French" },
+  ],
+  weights: [
+    { code: "KG", value: "Kilogram" },
+    { code: "LBS", value: "Pound" },
+  ],
+  dimensions: [
+    { code: "CM", value: "CentiMeter" },
+    { code: "MT", value: "Meter" },
+  ],
+  currencyFormat: [
+    { code: "1000000.00", value: "1000000.00" },
+    { code: "1,000,000.00", value: "1,000,000.00" },
+  ],
+  numberFormat: [
+    { code: "1000000", value: "1000000" },
+    { code: "1,000,000", value: "1,000,000" },
+  ],
+  dateFormat: [
+    { code: "MMDDYYYY", value: "MM/DD/YYYY" },
+    { code: "MMDDYY", value: "MM/DD/YY" },
+    { code: "DDMMYYYY", value: "DD/MM/YYYY" },
+  ],
+};
+
+export const userPreference = {
+  country: { code: "IN", value: "India" },
+  currency: { code: "INR", value: "Indian Rupee" },
+  dateFormat: { code: "MMDDYYYY", value: "MM/DD/YYYY" },
+  currencyFormat: { code: "1,000,000.00", value: "1,000,000.00" },
+  language: { code: "EN", value: "English" },
+  dimension: { code: "CM", value: "CentiMeter" },
+  weight: { code: "KG", value: "Kilogram" },
+  numberFormat: { code: "1,000,000", value: "1,000,000" },
+};
